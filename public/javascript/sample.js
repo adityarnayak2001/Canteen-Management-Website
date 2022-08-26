ab11bdbaa898005f4b79d4aadea59fc93406c824
Aditya R Nayak <adityarnayak2001@users.noreply.github.com>
2022-08-26 17:04:44 +0530

    some changes unknown

--- public/javascript/sample.js
+++ public/javascript/sample.js
@@ -4,28 +4,32 @@ if (document.readyState == 'loading') {
     ready()
 }
 function ready() {
-    var addCartItemButtons = document.getElementsByClassName('btn-add')
+    var addCartItemButtons = document.getElementsByClassName('btn-add');
     for (var i = 0; i < addCartItemButtons.length; i++) {
         var button = addCartItemButtons[i]
-        button.addEventListener('click', add)
+        button.addEventListener('click', add);
     }
-    
 }
-// function removeCartItem(event) {
-//     var buttonClicked = event.target
-//     buttonClicked.parentElement.getElementsByClassName()
-//     updateCartTotal()
-// }
-var add = (function(){
-    var buttonClicked = event.target
-   var add =  buttonClicked.parentElement.getElementsByClassName("abc");
-    updateCartTotal()
-    var q = 0,price=0;
-    var quantity = document.getElementsByClassName("abc")[0];
-    var price = document.getElementsByClassName("cdf")[0];
-    function a(){
+function add(event){
+    var buttonClicked = event.target;
+    var adds =  buttonClicked.parentElement.parentElement;
+    var q = 0;
+    var o_price = parseInt(adds.getElementsByClassName("price")[0].innerText);
+    var quantity = adds.getElementsByClassName("abc")[0];
+    var price = adds.getElementsByClassName("cdf")[0];
+    console.log(quantity.innerText+"--------------"+price.innerText)
+    var sum = (function a(){
         ++q;
         quantity.innerText = ""+q;
-    }
-    return a;    
-})(event);
+        price.innerText = ""+o_price*q;
+    })();
+    return sum;
+    // var updateClickCount = (function(){
+    //     var counter = 0;
+    
+    //     return function(){
+    //         ++counter;
+    //         document.getElementById("spnCount").innerHTML = counter;
+    //     }
+    // })();    
+}
